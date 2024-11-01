2ac94842dedf7f83084d334b0f7fbfec1e941b7b
shevelev.rv <shevelevroman12@gmail.com>
2024-11-01 23:00:19 +1000

    pep-8

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -7,8 +7,8 @@ router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='posts')
 router.register(r'groups', GroupViewSet, basename='groups')
 router.register(r'posts/(?P<post_id>\d+)/comments',
-				CommentViewSet,
-				basename='comments')
+                CommentViewSet,
+                basename='comments')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
